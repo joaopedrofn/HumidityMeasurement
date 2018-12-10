3b98e3f3b6a9f835ae07090307126485fcee581f
unknown <victorgfb80@gmail.com>
2018-12-10 08:28:30 -0300

    Project created

---
+++ HumidityMeasurement/main.c
@@ -0,0 +1,18 @@
+/*
+ * HumidityMeasurement.c
+ *
+ * Created: 10/12/2018 08:27:23
+ * Author : jzrhard06
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
