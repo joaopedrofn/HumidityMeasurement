8c8040175d6e15db3f50f5c6ed61a168304ba1cf
unknown <victorgfb80@gmail.com>
2018-12-10 09:51:34 -0300

    Trying to read DHT22

--- HumidityMeasurement/main.c
+++ HumidityMeasurement/main.c
@@ -6,13 +6,45 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
 
+#ifndef F_CPU
+
+#define  F_CPU 16000000UL
+
+#endif
 
 int main(void)
 {
+	
     /* Replace with your application code */
     while (1) 
     {
+		uint16_t temp;
+		uint16_t humidity;
+		_delay_ms(20000);
+		DDRD = (1 << PORTD7);
+		PORTD = (1 << PORTD7);
+		_delay_ms(25);
+		PORTD = 0;
+		_delay_ms(2);
+		cli();
+		PORTD = (1 << PORTD7);
+		_delay_ms(4);
+		DDRD = 0;
+		_delay_ms(1);
+		_delay_us(16);
+		for(int i = 0; i < 16; i++){
+			temp[i] = PORTD[PORTD7];
+			_delay_us(5);
+		}
+		for(int i = 0; i < 16; i++){
+			humidity[i] = PORTD[PORTD7];
+			_delay_us(5);
+		}
+		
+				
     }
 }
 
