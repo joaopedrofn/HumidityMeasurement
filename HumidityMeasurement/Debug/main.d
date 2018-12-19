10ab60086407cb039986a511ec2f4ecb10bfee95
joaopedrofn <joaopedrofn@gmail.com>
2018-12-19 09:40:19 -0300

    Digital IO changed

--- HumidityMeasurement/Debug/main.d
+++ HumidityMeasurement/Debug/main.d
@@ -13,7 +13,9 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -44,3 +46,7 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
--- HumidityMeasurement/main.c
+++ HumidityMeasurement/main.c
@@ -14,6 +14,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <stdlib.h>
 
 
 
@@ -32,42 +33,72 @@ void USART_send(unsigned char data){
 int main(void)
 {
 	USART_init();
-	
+	DDRD |= (1<<PORTD7);
+	PORTD |= (1<<PORTD7);
     while (1) 
     {
-		uint8_t tempL;
-		uint8_t tempH;
+		int8_t tempL;
+		int8_t tempH;
 		uint8_t humidityL;
 		uint8_t humidityH;
+		uint8_t retries, i;
+		int8_t j;
+		retries = i =j =0;
+		uint8_t timeout = 0;
 		_delay_ms(2000);
-		DDRD = (1 << PORTD7);
-		PORTD = (1 << PORTD7);
-		_delay_ms(1);
-		PORTD = 0;
-		_delay_us(20);
+		
+		//START READING	
+		PORTD |= (1<<PORTD7);
+		_delay_ms(250);
+		
+		//SET LOW FOR 20 ms
+		DDRD &= ~(1<<PORTD7);
+		PORTD &= ~(1<<PORTD7);
+		_delay_ms(20);
+		
+		//TURN OFF INTERRUPTIONS (CRITICAL PHASE)
 		cli();
-		DDRD = 0;
-		_delay_ms(10);
-		_delay_us(160);
+		
+		//END START SIGNAL
+		PORTD |= (1<<PORTD7);
+		_delay_us(40);
+		
+		//START TO READ
+		DDRD |= (1<<PORTD7);
+		PORTD |= (1<<PORTD7);
+		_delay_us(10);
+		
+		//WAIT 80 us OF LOW LEVEL
+		//while(!(PORTD & (1<<PORTD7)));
+		_delay_us(80);
+		
+		//WAIT 80 us OF HIGH LEVEL
+		//while((PORTD & (1<<PORTD7)));
+		_delay_us(80);
+		
+		//READ BYTES
 		for(int i = 0; i < 8; i++){
-			tempL = tempL | (((PORTD >> PORTD7) & 0x80)<<i);
+			tempL = tempL | (PORTD & (1<<PORTD7));
 			_delay_us(50);
 		}
 		for(int i = 0; i < 8; i++){
-			tempH = tempH | (((PORTD >> PORTD7) & 0x80)<<i);
+			tempH = tempH | (PORTD & (1<<PORTD7));
 			_delay_us(50);
 		}
 		for(int i = 0; i < 8; i++){
-			humidityL = humidityL | (((PORTD >> PORTD7) & 0x80)<<i);
+			humidityL = humidityL | (PORTD & (1<<PORTD7));
 			_delay_us(50);
 		}
 		for(int i = 0; i < 8; i++){
-			humidityH = humidityH | (((PORTD >> PORTD7) & 0x80)<<i);
+			humidityH = humidityH | (PORTD & (1<<PORTD7));
 			_delay_us(50);
 		}
+		
+		//ENABLE INTERRUPTIONS (END OF CRITICAL PHASE)
 		sei();
-		USART_send(tempH);
-		USART_send(tempL);
+		
+		USART_send(tempH+48);
+		USART_send(tempL+48);
 				
     }
 }
