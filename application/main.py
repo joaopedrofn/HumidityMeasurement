9531b4b858483be1f71395005de965c3440c9353
joaopedrofn <joaopedrofn@gmail.com>
2019-02-20 09:56:49 -0300

    software finished

--- application/main.py
+++ application/main.py
@@ -1,22 +1,71 @@
 from serial import Serial
 import time
 import csv
-import random
+import matplotlib.pyplot as plt
+import matplotlib.dates as dates
 from datetime import datetime
+from threading import Thread
+import numpy as np
 
-# serial = Serial('COM14')
+collect = False
 
-while True:
-    time.sleep(5)
-    with open('db.csv', 'a', newline='') as csvfile:
-        data = []
-        data.append(datetime.now())
-        # serial.write(bytes('a', 'utf-8'))
-        data.append(random.randint(0, 1000)/10)
-        # data[0] = str(serial.read(4))
-        # serial.write(bytes('s', 'utf-8'))
-        # data[1] = str(serial.read(4))
-        data.append(random.randint(0, 1023))
-        csv.writer(csvfile).writerow(data)
+def plot():
+    with open('db.csv', 'rt') as csvfile:
+        date =[]
+        air = []
+        soil = []
+        for row in csv.reader(csvfile):
+            date.append(dates.date2num(datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f')))
+            air.append(float(row[1])*0.1)
+            soil.append(((float(row[2])-1023)/(-1023))*100)
+    fig1 = plt.figure()
+    fig2 = plt.figure()
+    pl1 = fig1.add_subplot(1,1,1)
+    pl2 = fig2.add_subplot(1,1,1)
+    pl1.plot_date(date, air, 'b-')
+    pl2.plot_date(date, soil, 'b-')
+    plt.show()
+
+def collectData():
+    serial = Serial('COM14')
+    global collect
+    while collect:
+        time.sleep(5)
+        with open('db.csv', 'a', newline='') as csvfile:
+            data = []
+            data.append(datetime.now())
+            serial.write(bytes('a', 'utf-8'))
+            data.append(str(serial.read(4))[2:-5])
+            serial.write(bytes('s', 'utf-8'))
+            data.append(str(serial.read(4)).replace('\'', '').replace('b', '').replace('\\x00', ''))
+            csv.writer(csvfile).writerow(data)
 
 
+def listenKeyboard():
+    op = 0
+    while op != '1' and op != '2':
+        print("Digite a opção desejada:\n")
+        print("1. Coletar dados.\n")
+        print("2. Plotar Gráficos.\n")
+        op = input("Digite: ")
+        if op == '1':
+            global collect
+            collect = True
+            collectThread.start()
+            q = ''
+            while(q != 'q'):
+                q = input("Para sair e plotar os gráficos digite 'q' e tecle 'enter'")
+                if(q): 
+                    collect = False
+                    plot()
+                    exit()
+        elif op == '2':
+            plot()
+            exit()
+
+
+
+collectThread = Thread(target=collectData)
+listenThread = Thread(target=listenKeyboard)
+
+listenThread.start()
